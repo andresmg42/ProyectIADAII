27e12a91f9659dd42418608c866815c133bad076
DanielTrujillo01 <jose.trujillo@correounivalle.edu.co>
2025-10-12 19:53:46 -0500

    Limpieza de algunas archivos y README añadido

--- view.py
+++ view.py
@@ -27,7 +27,7 @@ class View:
                 return
 
     def load_data_menu(self):
-        filename = input("Escriba el nombre del archivo (en carpeta inputs): ")
+        filename = input("Escriba el nombre del archivo (en carpeta inputs Ejemplos: test_1.txt): ")
         try:
             self.controller.load_data_txt(f"./inputs/{filename}")
             print("\nDatos cargados correctamente!!!")
@@ -64,7 +64,7 @@ class View:
         print(f"\nSolución usando {self.controller.executed_algorthrim}:")
         print("----------------------------------")
         self.controller.print_solution()
-        self.controller.write_solution()
+        self.controller.write_FB_solution()
         print("----------------------------------\n")
 
     def exit_app(self):
